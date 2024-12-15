44d64517ea9a90914a6aaa972666b8c790cbb1f7
Patrício <patricioanjosjf@gmail.com>
2024-12-15 11:39:15 -0300

    Add project files.

---
+++ Controllers/ForumController.cs
@@ -0,0 +1,40 @@
+﻿using Forun.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Forun.Controllers
+{
+    public class ForumController : Controller
+    {
+        // Lista estática para armazenar os dados
+        private static List<Comment> _comments = new List<Comment>
+        {
+            new Comment
+            {
+                Id = 1,
+                Content = "Este é o primeiro comentário.",
+                Author = "Usuário1",
+                CreatedAt = DateTime.Now,
+                Replies = new List<Comment>()
+            },
+            new Comment
+            {
+                Id = 3,
+                Content = "Este é o segundo comentário.",
+                Author = "Usuário3",
+                CreatedAt = DateTime.Now,
+                Replies = new List<Comment>()
+            }
+        };
+
+        // GET: Forum
+        public ActionResult Index()
+        {
+            var mainComments = _comments.Where(c => c.ParentId == null).ToList();
+            return View(mainComments);
+        }
+    }
+}
---
+++ Models/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Forun.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        // Relacionamento com respostas
+        public List<Comment> Replies { get; set; } = new List<Comment>();
+
+        // Identificador para diferenciar respostas de comentários principais
+        public int? ParentId { get; set; }
+    }
+}
